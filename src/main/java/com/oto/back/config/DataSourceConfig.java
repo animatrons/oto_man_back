81ab275832c6f5dd514a3965f30b2c33227d1a1c
Amin <medamineharwel@outlook.com>
2022-09-21 16:48:57 +0100

    added DataSourceConfig with yml properties file with Hikari Data source template

---
+++ src/main/java/com/oto/back/config/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.oto.back.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.main")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    public JdbcTemplate jdbcTemplate(HikariDataSource hikariDataSource) {
+        return new JdbcTemplate(hikariDataSource);
+    }
+}
